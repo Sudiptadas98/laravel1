63ea4745d98e3b9bf3376f71938df81ca96b68cd
Sudipta Das <sudiptodas033@gmail.com>
2020-07-30 22:35:05 +0530

    day05

--- project001/app/Http/Controllers/ArticlesController.php
+++ project001/app/Http/Controllers/ArticlesController.php
@@ -17,11 +17,11 @@ class ArticlesController extends Controller
         return view('articles.index', ['articles' => $articles]);
     }
 
-    public function show($id)
+    public function show(Article $article)
     {
         // show a single resource
        
-        $article = Article::find($id);
+        // $article = Article::findorfail($id);
 
         return view('articles.show', ['article' => $article]);
     }
@@ -36,51 +36,62 @@ class ArticlesController extends Controller
 
     public function store()
     {
-        request()->validate([
-            'title' => 'required',
-            'expert' => 'required',
-            'body' => 'required'   
-        ]);
-
         // persist the new resource
-        $article = new Article();
+        
+        Article::create($this->validateArticle());
 
-        $article->title = request('title');
-        $article->excerpt = request('excerpt');
-        $article->body = request('body');
 
-        $article->save();
+        // $article = new Article();
 
-        return redirect('/articles');
+        // $article->title = request('title');
+        // $article->excerpt = request('excerpt');
+        // $article->body = request('body');
+
+        // $article->save();
+            //OR>>
+        // Article::create([
+        //     'title' => request('title'),
+        //     'excerpt' => request('excerpt'),
+        //     'body' => request('body')
+        // ]);
+            //OR>>
+        // Article::create($validatedAttributes);
+
+        return redirect(route('articles.index'));
     }
 
-    public function edit($id)
+    public function edit(Article $article)
     {
         // show a view to edit an existing resource
 
         // find the article associated with the id
-        $article = Article::find($id);
+        // $article = Article::find($id);
         return view('articles.edit', ['article' => $article]);
     }
 
-    public function update($id)
+    public function update(Article $article)
     {
-        request()->validate([
-            'title' => 'required',
-            'expert' => 'required',
-            'body' => 'required'   
-        ]);
+        $article->update($this->validateArticle());
         // persist the edited resource
 
-        $article = Article::find($id);
+        // $article = Article::find($id);
 
-        $article->title = request('title');
-        $article->excerpt = request('excerpt');
-        $article->body = request('body');
+        // $article->title = request('title');
+        // $article->excerpt = request('excerpt');
+        // $article->body = request('body');
 
-        $article->save();
+        // $article->save();
         
-        return redirect('/articles/' . $article->id);
+        return redirect($article->path());
+    }
+
+    protected function validateArticle()
+    {
+        return request()->validate([
+            'title' => 'required',
+            'excerpt' => 'required',
+            'body' => 'required'   
+        ]);
     }
 
     public function destroy()
